325f3a2939c2d6c9bee1ac81f9dd662755ea93ff
Kethan Koushik <kethantechspace@gmail.com>
2026-02-05 13:02:30 +0530

    fix: ensure en_core_web_sm is downloaded during deployment

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+set -e
+
+echo "Installing spaCy models..."
 python -m spacy download en_core_web_sm
-pip install https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.5.4/en_core_sci_md-0.5.4.tar.gz
+
+echo "Models installed successfully!"
